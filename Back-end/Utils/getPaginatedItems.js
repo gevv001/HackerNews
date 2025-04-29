49429c20cc01962853efdc26783da8968259c4c9
Gev <gevorg.120.torosyan@gmail.com>
2025-04-29 22:17:36 +0400

    paginating of comments

--- Back-end/Utils/getPaginatedItems.js
+++ Back-end/Utils/getPaginatedItems.js
@@ -1,6 +1,15 @@
+import { CommentModel } from "../Models/comment.js";
 import { PostModel } from "../Models/post.js";
 
 export const getPaginatedItems = async (type, page, sortBy = 'createdAt') => {
+    if (type == 'comment') {
+        return await CommentModel.find()
+        .sort({ [sortBy]: -1 })
+        .skip((page - 1) * 30)
+        .limit(30)
+        .populate('author', 'username')
+    }
+    
     return await PostModel.find({type})
         .sort({ [sortBy]: -1 })
         .skip((page - 1) * 30)
