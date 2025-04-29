aa653aa22cc8fbd3a165597b056980b0e80852e3
Gev <gevorg.120.torosyan@gmail.com>
2025-04-29 20:09:53 +0400

    added cors

--- Back-end/app.js
+++ Back-end/app.js
@@ -4,9 +4,11 @@ import mongoose from "mongoose";
 import connectDB from "./Config/db.js";
 import apiRoutes from "./Routes/apiRoute.js";
 import errorHandler from "./Middleware/errorHandler.js";
+import cors from 'cors'
 configDotenv();
 let app = express();
 
+app.use(cors());
 app.use(express.json())
 app.use('/api', apiRoutes)
 app.use(errorHandler)
